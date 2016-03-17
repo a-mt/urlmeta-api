faf7dac19eae51f694f5af2e88f9a521761ba089
Moin Uddin <me@moin.im>
2016-03-17 18:03:05 +0500

    some minor changes

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-require('newrelic');
+var
+	request = require('request'),
+	cheerio = require('cheerio'),
+	http    = require('http'),
+	uri     = require('url'),
+	valid   = require('validator');
 
-var request = require('request'),
-cheerio = require('cheerio'),
-http = require('http'),
-uri = require('url'),
-valid = require('validator');
+var
+	$        = null,	// to Hold cheerio DOM object
+	response = null,
+	metas    = null,
+	resBack  = {},
+	timeOut  = 5000,
+	onlyHead = false,
 
-// var mongodb = require('mongodb');
-
-var $ = null,
-response = null,
-resBack = {},
-timeOut = 5000,
-onlyHead = false;
-
-// var mongoURI = process.env.MONGOLAB_URI;
+	PORT     = process.env.PORT || 9615;
 
 function fullURL (url, path) {
 	return uri.resolve( url, path );
@@ -37,34 +36,42 @@ function getBody (url) {
 
 			try {
 				$ = cheerio.load( cheerio( 'head', body ).html() );
-				var metas = $( 'meta[property], meta[name], meta[itemprop]' );
+				metas = $( 'meta[property], meta[name], meta[itemprop]' );
 			} catch (e) {
-				respond({ error: true, reason: 'Could not parse HTML of website.', code: 6  });
+				return respond({ error: true, reason: 'Could not parse HTML of website.', code: 6  });
 			}
 
-			if(metas && metas.length > 0) {
+			if(metas !== null && metas.length > 0) {
 
 				var meta = {};
 				metas.each(function() {
-					if( $( this ).attr('name') == 'urlmeta'  &&  $( this ).attr('content') )
+					if( $( this ).attr('name') == 'urlmeta'  &&  $( this ).attr('content') ) {
 						respond({ error: true, reason: 'Website does not allow crawling.', code: 3 });
-					else if( $( this ).attr('property') == 'og:title' || $( this ).attr('name') == 'twitter:title' )
+					} else if( $( this ).attr('property') == 'og:title' ||
+										$( this ).attr('name') == 'twitter:title' ) {
 						meta.title = $( this ).attr('content');
-					else if( $( this ).attr('property') == 'og:description' || $( this ).attr('name') == 'twitter:description' || $( this ).attr('itemprop') == 'description' || $( this ).attr('name') == 'description' )
+					} else if( $( this ).attr('property') == 'og:description' ||
+										 $( this ).attr('name') == 'twitter:description' ||
+										 $( this ).attr('itemprop') == 'description' ||
+										 $( this ).attr('name') == 'description' ) {
 						meta.description = $( this ).attr('content');
-					else if( $( this ).attr('property') == 'og:image' || $( this ).attr('name') == 'twitter:image' || $( this ).attr('itemprop') == 'image' )
+					} else if( $( this ).attr('property') == 'og:image' ||
+										 $( this ).attr('name') == 'twitter:image' ||
+										 $( this ).attr('itemprop') == 'image' ) {
 						meta.image = fullURL( url, $( this ).attr('content') );
+					}
 				});
 
-				resBack.title = meta.title;
+				resBack.title       = meta.title;
+				resBack.image       = meta.image;
 				resBack.description = meta.description;
-				resBack.image = meta.image;
 
 			}
 
 			$('link[rel]').each(function() {
-				if( $( this ).attr( 'rel' ) == 'shortcut icon' || $( this ).attr( 'rel' ) == 'icon' )
+				if( $( this ).attr( 'rel' ) == 'shortcut icon' || $( this ).attr( 'rel' ) == 'icon' ) {
 					resBack.favicon = fullURL( url, $( this ).attr( 'href' ) );
+				}
 			});
 
 			var feed = $('link[rel=alternate]');
@@ -79,10 +86,10 @@ function getBody (url) {
 				resBack.title = $('title').text();
 			}
 
-			respond();
+			return respond();
 
 		} else if (err) {
-			respond({ error: true, reason: 'Could not parse HTML of website.', code: 6 });
+			return respond({ error: true, reason: 'Could not parse HTML of website.', code: 6 });
 		}
 
 	});
@@ -100,8 +107,9 @@ function getHead (url){
 	}).on('response', function (response) {
 	  if (response.statusCode == 200) {
 
-	  	if(response.headers['urlmeta'] && response.headers['urlmeta'] == 'no')
+	  	if(response.headers.urlmeta && response.headers.urlmeta == 'no') {
 	  		respond({ error: true, reason: 'Website does not allow crawling.', code: 3 });
+			}
 
 	  	var cont = response.headers['content-type'];
 	  	resBack = {
@@ -110,18 +118,18 @@ function getHead (url){
 	  			size: response.headers['content-length']
 	  		};
 	  	if( cont.substr(0, 9) == 'text/html' && !onlyHead ) {
-	  		getBody(url);
+	  		return getBody(url);
 	  	} else {
 	  		onlyHead = true;
-	  		respond();
+	  		return respond();
 	  	}
 
 	  } else if (response.statusCode >= 400) {
-	  	respond({ error: true, reason: 'Could not find what you were looking for.', httpCode: response.statusCode, code: 4 });
+	  	return respond({ error: true, reason: 'Could not find what you were looking for.', httpCode: response.statusCode, code: 4 });
 	  }
 
 	}).on('error', function(err) {
-		respond({ error: true, reason: 'Request time out. Website could not be reached in time.', code: 5 });
+		return respond({ error: true, reason: 'Request time out. Website could not be reached in time.', code: 5 });
 	});
 
 }
@@ -139,25 +147,27 @@ function respond (r) {
 		sendBack.result.code   = r.code;
 		sendBack.result.reason =  r.reason || 'Unknown';
 
-		if( sendBack.result.reason != 'Unknown' )
+		if( sendBack.result.reason != 'Unknown' ) {
 			sendBack.result.reason += ' Read docs here: https://urlmeta.org/dev-api.html';
+		}
 
-		if( r.httpCode )
+		if( r.httpCode ) {
 			sendBack.result.httpCode = r.httpCode;
+		}
 
 	} else {
 		sendBack.meta = {};
 		sendBack.result.status = 'OK';
 
-		// omit ;charset=utf-8
 		if(r.type && r.type.substr(0, 9) == 'text/html')
 			r.type = 'text/html';
 
 		sendBack.meta = r;
 	}
 
-	if(onlyHead)
+	if(onlyHead) {
 		sendBack.result.onlyHead = true;
+	}
 
 	response.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
 	response.end( JSON.stringify( sendBack ) );
@@ -166,10 +176,11 @@ function respond (r) {
 function startURL (url) {
 	if( valid.isURL(url, { protocols: ['http','https'], require_protocol: true } ) ) {
 		getHead(url);
-	} else if( url == undefined )
+	} else if( url === undefined ) {
 		respond( { error: true, reason: "Parameter URL not found.", code: 1 } );
-	else
+	} else {
 		respond( { error: true, reason: "Provided URL '"+ url +"' is not valid", code: 2 } );
+	}
 }
 
 
@@ -177,16 +188,17 @@ function init (req, res) {
 
 	var query = uri.parse(req.url, true).query;
 
-	onlyHead = (query.onlyHead != undefined);
+	onlyHead = (query.onlyHead !== undefined);
 	response = res;
 
-	if(query.url && query.url.indexOf('api.urlmeta.org') > 0)
-		respond({
+	if(query.url && query.url.indexOf('api.urlmeta.org') > 0) {
+		return respond({
 			error: true,
 			reason: "Somebody is getting cocky!"
 		});
-
-	startURL( query.url );
+	} else {
+		return startURL( query.url );
+	}
 }
 
 var serv = http.Server();
@@ -196,5 +208,5 @@ http.createServer(function (req, res) {
 
 	init( req, res );
 
-}).listen(process.env.PORT || 9615);
-console.log('Alhamdulilah, Running...');
+}).listen(PORT);
+console.log('Running on port', PORT);
